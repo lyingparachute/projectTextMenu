123ba309540a82ebdc333495a6ac72a4106e1926
Igor Bykowski <47899250+lyingparachute@users.noreply.github.com>
2021-10-01 15:47:44 +0200

    Add files via upload
    Initial commit

---
+++ src/pl/infoshare/Main.java
@@ -0,0 +1,12 @@
+package pl.infoshare;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Interface anInterface = new Interface();
+        anInterface.defaultInterface();
+    }
+
+}
